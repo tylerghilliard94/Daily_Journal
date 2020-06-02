f33943663f107cea1becf19c65270406e865d816
Tyler Hilliard <tylerghilliard@gmail.com>
2020-06-02 15:00:13 -0500

    Part 3

---
+++ scripts/Journal/journal.js
@@ -0,0 +1,11 @@
+const journalConverter = (journalObject) => {
+    const journalHTMLRepresentation = ` <section class="entry">
+    <h1>${journalObject.Concepts}</h1>
+    <p> 
+        ${journalObject.Content}<br>
+        ${journalObject.Date}<br>
+        Mood: <strong>${journalObject.Mood}</strong>
+    </p>
+    </section>`
+    return journalHTMLRepresentation
+}
