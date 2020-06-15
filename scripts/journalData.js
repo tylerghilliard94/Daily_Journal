a80d93ef9c0fc3d9454e2c6f740c3d151bd5e387
Tyler Hilliard <tylerghilliard@gmail.com>
2020-06-15 10:40:34 -0500

    Module conversion

--- scripts/journalData.js
+++ scripts/journalData.js
@@ -2,15 +2,24 @@ var journalDataArray = [
 
 ]
 
-const getJournalData = () => {
-    return fetch("http://localhost:8088/entries").then(
-        (response) => {
-            return response.json()
-        }
-    )
-    .then(
-        (arrayOfEntries) => {
-            journalDataArray = arrayOfEntries
-        }
-    )
+// const getJournalData = () => {
+//     return fetch("http://localhost:8088/entries").then(
+//         (response) => {
+//             return response.json()
+//         }
+//     )
+//     .then(
+//         (arrayOfEntries) => {
+//             journalDataArray = arrayOfEntries
+//         }
+//     )
+// }
+
+const API = {
+    getJournalEntries () {
+        return fetch("http://localhost:8088/entries")
+        .then(response => response.json())
+    }
 }
+
+export default API
